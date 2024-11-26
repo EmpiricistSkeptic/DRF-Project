7fb3335b2049b5975fb9ec53a6910fe1fc68e379
EmpiricistSkeptic <131171004+EmpiricistSkeptic@users.noreply.github.com>
2024-11-26 15:33:52 +0000

    some changes

--- api/models.py
+++ api/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks', default=1)
     title = models.TextField()
     description = models.TextField(null=True, blank=True)
-    deadline = models.DateTimeField("default=datetime=now")
+    deadline = models.DateTimeField(default=datetime(2024,11,10,23,59))
     completed = models.BooleanField(default=False)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
--- api/tasks.py
+++ api/tasks.py
@@ -5,7 +5,7 @@ from .models import Task
 @shared_task
 def check_task_deadline():
     now = timezone.now()
-    overdue_tasks = Task.objects.filter(completed=False, deadline__1t=now)
+    overdue_tasks = Task.objects.filter(completed=False, deadline__lt=now)
     for task in overdue_tasks:
         user_profile = task.user.profile
         user_profile.points = max(0, user_profile.points - 10)
