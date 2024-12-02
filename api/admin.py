23bfc551060442de93adea2f6274ebfe3727cd4e
EmpiricistSkeptic <131171004+EmpiricistSkeptic@users.noreply.github.com>
2024-12-02 16:03:05 +0000

    some changes

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Profile, Message, Friendship, Notification, Group, GroupMessage, PomodoroTimer, EducationalContent, ConsumedCalories
 
 admin.site.register(Task)
+admin.site.register(Profile)
+admin.site.register(Message)
+admin.site.register(Friendship)
+admin.site.register(Notification)
+admin.site.register(Group)
+admin.site.register(GroupMessage)
+admin.site.register(PomodoroTimer)
+admin.site.register(EducationalContent)
+admin.site.register(ConsumedCalories)
+
