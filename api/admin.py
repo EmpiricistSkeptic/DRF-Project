15608d0a4a0d5b5cd47ea6da4ccf6250b13b3717
EmpiricistSkeptic <131171004+EmpiricistSkeptic@users.noreply.github.com>
2025-04-16 18:28:24 +0000

    deleted models

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Task, Profile, Message, Friendship, Notification, Group, GroupMessage, PomodoroTimer, EducationalContent, ConsumedCalories, Achievement
+from .models import Task, Profile, Message, Friendship, Notification, Group, GroupMessage, ConsumedCalories, Achievement
 
 admin.site.register(Task)
 admin.site.register(Profile)
@@ -8,8 +8,6 @@ admin.site.register(Friendship)
 admin.site.register(Notification)
 admin.site.register(Group)
 admin.site.register(GroupMessage)
-admin.site.register(PomodoroTimer)
-admin.site.register(EducationalContent)
 admin.site.register(ConsumedCalories)
 admin.site.register(Achievement)
 
